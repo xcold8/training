2ad43296fe89a48182704fc3ba7446c0ecd02d30
xcold8 <haim@phanatic.io>
2017-03-23 18:09:44 +0200

    changed design and HTML

--- script.js
+++ script.js
@@ -1,14 +1,61 @@
-$('#main .tasks_wording .before').click(function(){
-	if ( $(this).hasClass('before') ) {
-			make_After();
-		}
-	if ($this).hasClass('after'){
-		make_Before();
-	}
 
-function make_After(){
-	$('#main .tasks_wording .before').removeClass('before').addClass('after');
-};
-function make_Before(){
-	$('#main .tasks_wording').removeClass('after').addClass('before');
-};
+// adds a string to the list as an item
+function addItem(text_inp) {
+	if (!text_inp)
+		return;
+	$('#main .tasks_wording .ulist').append('<li class="new">'+ text_inp +'</li>');
+}
+
+// triggers when an item is clicked
+
+// $('#main .tasks_wording .ulist li').click(function(){
+
+$(document).on('click', '#main .tasks_wording .ulist li', function(){
+	if ($(this).hasClass('done')) {
+		mark_undone(this);
+	}
+	else {
+		mark_done(this);
+	}
+});
+
+
+
+$('#main .task_input .btn_add').click(function(){
+	var $txt_input = $(this).parents('.task_input').find('.text_input');
+	addItem($txt_input.val());
+	$txt_input.val('');
+});
+
+
+$('#main .task_input').keypress(function(e){
+
+	if (e.keyCode == 13) {
+		// enter
+		var $txt_input = $(this).find('.text_input');
+		addItem($txt_input.val());
+		$txt_input.val('');
+	}
+});
+
+$(document).on('click', '#main .task_clear .clear_btn', function(){
+	clear_Done();
+});
+
+function mark_done(el){
+	$(el).removeClass('new').addClass('done');
+}
+
+function mark_undone(el){
+	$(el).removeClass('done').addClass('new');
+}
+function clear_Done(){
+	var $done_Count = $('#main .tasks_wording .ulist .done').length;
+	console.log($done_Count);
+	if ($done_Count === 0) { 
+		return;}
+		else {
+		 		$('#main .tasks_wording .ulist .done').remove();
+
+		 }
+	}
